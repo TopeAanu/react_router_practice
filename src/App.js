b7cf92faf45e433a91aa30e734f757b0c9b2ea19
TopeAanu <topefasasi99@gmail.com>
2025-01-09 00:34:08 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { Routes, Route } from "react-router-dom";
-import Welcome from "./components/Welcome";
-import Products from "./components/Products";
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+import MainHeader from "./components/MainHeader";
 
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path='/welcome' element={<Welcome />} />
-        <Route path='/products' element={<Products />} />
-      </Routes>
+      <MainHeader />
+      <main>
+        <Routes>
+          <Route path="/welcome" element={<Welcome />} />
+          <Route path="/products" element={<Products />} />
+        </Routes>
+      </main>
     </div>
   );
 }
